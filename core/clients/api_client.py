bdfec003c6f217e48fb6b5e6c4170ccb09b50d5c
Evgenii <filatov.j95@gmail.com>
2026-01-27 20:04:44 +0300

    Добавил метод get_booking_by_id

--- core/clients/api_client.py
+++ core/clients/api_client.py
@@ -2,6 +2,9 @@ import requests
 import os
 from dotenv import load_dotenv
 from core.settings.environments import Environment
+from core.clients.endpoints import Endpoints
+from core.settings.config import Users, Timeouts
+import allure
 
 load_dotenv()
 
@@ -14,7 +17,8 @@ class APIClient:
             raise ValueError(f'Unsupported environment value: {environment_str}')
 
         self.base_url = self.get_base_url(environment)
-        self.headers = {
+        self.session = requests.Session()
+        self.session.headers = {
             'Content-Type': 'application/json'
         }
 
@@ -28,14 +32,70 @@ class APIClient:
 
     def get(self, endpoint, params=None, status_code=200):
         url = self.base_url + endpoint
-        response = requests.get(url, headers=self.headers, params=params)
+        response = requests.get(url, headers=self.session.headers, params=params)
         if status_code:
             assert response.status_code == status_code
         return response.json()
 
     def post(self, endpoint, data=None, status_code=200):
         url = self.base_url + endpoint
-        response = requests.post(url, headers=self.headers, json=data)
+        response = requests.post(url, headers=self.session .headers, json=data)
         if status_code:
             assert response.status_code == status_code
         return response.json()
+
+    def ping(self):
+        with allure.step('Ping api client'):
+            url = f'{self.base_url}{Endpoints.PING_ENDPOINT}'
+            response = self.session.get(url)
+            response.raise_for_status()
+        with allure.step('Assert status code'):
+            assert response.status_code == 201, f'Expected status 201, but got {response.status_code}'
+        return response.status_code
+
+    def auth(self):
+        with allure.step('Getting authenticate'):
+            url = f'{self.base_url}{Endpoints.AUTH_ENDPOINT}'
+            payload = {'username': Users.USERNAME, 'password': Users.PASSWORD}
+            response = self.session.post(url, json=payload, timeout=Timeouts.TIMEOUT)
+            response.raise_for_status()
+        with allure.step('Assert status code'):
+            assert response.status_code == 200, f'Expected status 200, but got {response.status_code}'
+        token = response.json().get('token')
+        with allure.step('Updating header with authorization'):
+            self.session.headers.update({'Authorization': f'Bearer {token}'})
+
+    def get_booking_ids(self):
+        with allure.step('Getting booking ids'):
+            url = f'{self.base_url}{Endpoints.BOOKING_ENDPOINT}'
+            response = self.session.get(url)
+            response.raise_for_status()
+        with allure.step('Assert status code'):
+            assert response.status_code == 200, f'Expected status 200, but got {response.status_code}'
+        with allure.step('Validate response structure'):
+            assert isinstance(response.json(), list), f'Expected list, but got {type(response.json())}'
+            for i in response.json():
+                assert 'bookingid' in i, 'bookingid not found'
+        booking_ids = response.json()
+        return booking_ids
+
+    def get_booking_by_id(self, booking_id=None):
+        with allure.step('Getting bookingid'):
+            if booking_id is None:
+                booking_id = self.get_booking_ids()[0]['bookingid']
+        with allure.step('Getting booking by id'):
+            url = f'{self.base_url}{Endpoints.BOOKING_ENDPOINT}{booking_id}'
+            response = self.session.get(url)
+            response.raise_for_status()
+        with allure.step('Assert status code'):
+            assert response.status_code == 200, f'Expected status 200, but got {response.status_code}'
+            data = response.json()
+            assert isinstance(data[('firstname'), str])
+            assert isinstance(data[('lastname'), str])
+            assert isinstance(data[('totalprice'), float])
+            assert isinstance(data[('depositpaid'), bool])
+            assert isinstance(data[('bookingdates'), dict])
+            assert isinstance(data[('bookingdates')['checkin'], str])
+            assert isinstance(data[('bookingdates')['checkout'], str])
+            assert isinstance(data[('additionalneeds'), str])
+        return data
