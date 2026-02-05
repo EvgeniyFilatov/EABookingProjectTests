f814b46635d2eddaf05a78c14a2a3bddca1adecc
Evgenii <filatov.j95@gmail.com>
2026-02-05 16:05:14 +0300

    Написал негативные проверки на метод create_booking

--- core/clients/api_client.py
+++ core/clients/api_client.py
@@ -99,9 +99,9 @@ class APIClient:
         with allure.step('Creating booking'):
             url = f'{self.base_url}{Endpoints.BOOKING_ENDPOINT.value}'
             response = self.session.post(url, json=booking_data)
-            response.raise_for_status()
-        with allure.step('Assert status code'):
-            assert response.status_code == 200, f'Expected status 200, but got {response.status_code}'
+            # response.raise_for_status()
+        # with allure.step('Assert status code'):
+        #     assert response.status_code == 200, f'Expected status 200, but got {response.status_code}'
         return response
 
     def update_booking_by_id(self, booking_id, booking_data):
